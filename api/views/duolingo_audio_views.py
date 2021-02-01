dc339e76851fbf3d9675f6aeaba2bb1c82a1e4f7
benjenkinsv95 <benjenkinsv95@gmail.com>
2021-01-31 23:06:04 -0500

    Add pronunciation endpoint

---
+++ api/views/duolingo_audio_views.py
@@ -0,0 +1,51 @@
+import os
+import duolingo
+import json
+import time
+from rest_framework.views import APIView
+import inflect
+import re
+
+from django.http import HttpResponse
+from rest_framework.response import Response
+
+from ..models.mango import Mango
+from ..serializers import MangoSerializer, UserSerializer
+
+# Pull personal USERNAME and PASSWORD from .env credentials
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+
+# Sign in to duolingo API
+lingo  = duolingo.Duolingo(USERNAME, PASSWORD)
+DELAY = 0.5
+
+# Initialize engine used to pluralize words
+engine = inflect.engine()
+
+class DuoLingoAudio(APIView):
+    # disable authentication for this route
+    authentication_classes=[]
+    permission_classes=[]
+
+    def get(self, request):
+        """Index request"""
+        target_language = request.query_params.get('target_language')
+        word = request.query_params.get('word')
+
+        # Delay to make sure username is set before continuing
+        time.sleep(DELAY)
+
+        # It seems I can only get translations for the current language, so extract the
+        # ui_language - The language they are using in duolingo
+        # target_language - the language currently being learned in duo lingo
+        user_info = lingo.get_user_info()
+        source_language = user_info['ui_language']
+        target_language = lingo.get_abbreviation_of(target_language)
+        print('source_language', source_language, 'target_language', target_language)
+
+        audio_url = lingo.get_audio_url(word)
+
+        data = json.dumps({ 'audio_url': audio_url })
+
+        return Response(data)
